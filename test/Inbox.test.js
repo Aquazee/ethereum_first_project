04e2d9deace56548050f40bc87bab4b7ec4c0d19
Sudip Sharma <sudip.test12@gmail.com>
2022-12-11 13:26:59 +0530

    Added accounts test case

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,3 +4,16 @@ const ganache = require('ganache');
 const Web3 = require('web3');
 const { beforeEach, describe, it } = require('mocha');
 const web3 = new Web3(ganache.provider());
+
+let accounts;
+
+beforeEach(async () => {
+  // Getting Accounts from Ganache Ethereum Network
+  accounts = await web3.eth.getAccounts()
+})
+
+describe('Inbox', () => {
+  it('Received accounts', () => {
+    assert(0 < accounts.length, 'Failed Accounts import.')
+  })
+})
