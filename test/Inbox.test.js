f7bbbe6e4dc3889c61cea76601833e8618149a8b
Sudip Sharma <sudip.test12@gmail.com>
2022-12-11 13:10:53 +0530

    updated inbox test file

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,6 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache');
+const Web3 = require('web3');
+const { beforeEach, describe, it } = require('mocha');
+const web3 = new Web3(ganache.provider());
